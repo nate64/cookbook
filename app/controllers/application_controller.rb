ff46d2c16b38c8d09c70ee2745dca6b80e9cd3e9
Nate <nathanestes@gmail.com>
2018-10-01 10:19:31 -0600

    models, controllers and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+
+  #all controllers inherit from here
+
+  def render_error(obj)
+    render json: { errors: obj.errors.full_messages }, status: 422
+  end
+
 end
